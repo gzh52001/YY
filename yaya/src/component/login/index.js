fa4c12776ab960326a727b92445c4d3f78018784
Yaojie <1094119614@qq.com>
2020-07-20 09:20:08 +0800

    登录页面

--- yaya/src/component/login/index.js
+++ yaya/src/component/login/index.js
@@ -1,13 +1,29 @@
 import React,{Component} from 'react'
-// import '../style/main.scss'
-
+import '../../style/login.scss'
+import NormalLoginForm from '../../view/login-gn'
+import {
+    LeftOutlined,
+    EllipsisOutlined
+  } from '@ant-design/icons';
 
 class Login extends Component{
+    
+    chagod=()=>{
+            // console.log(this.props);
+            this.props.history.push('/mine')
+            }
     render(){
         return(
             <div className='login'> 
-
-          <span>hahahaha</span>
+            <div className='lo-tou'>
+                <div className='jt' onClick={this.chagod}><LeftOutlined /></div>
+                <div className='jit-p'><p>登录</p></div>
+                <div className='jt2'><EllipsisOutlined /></div>
+            </div>
+            <div className='zhmm'>
+                账号密码登录
+            </div>
+            <NormalLoginForm></NormalLoginForm>
           </div>
         )
     }
